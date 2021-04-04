4520ff62b5af2b3b211249b118a58d8f2ba11b99
shubham_97 <shubham.agarwal@clix.capital>
2021-04-05 01:55:10 +0530

    banner heading added from Locales

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -8,33 +8,22 @@ import { withTranslation } from 'utils/with-i18next';
 const BannerRoot = styled('div')`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
-  margin-top: 50px;
+  margin-top: 10px;
 `;
 
 const Title = styled('h1')`
-  font-size: 45px;
-`;
-
-const SubTitle = styled('h2')`
-  font-size: 20px;
-  margin: 0;
-  font-weight: 300;
-`;
-
-const Logo = styled('img')`
-  width: 110px;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: bold;
+  color: #2b416c;
 `;
 
 export function Banner({ t }) {
   return (
     <BannerRoot>
-      <Logo src="/static/images/reac-next-boilerplate-logo.svg" alt="react-next-boilerplate" />
-
       <Title>{t('title')}</Title>
-
-      <SubTitle>{t('subTitle')}</SubTitle>
     </BannerRoot>
   );
 }
