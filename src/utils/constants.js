fe6ba200bdfcf373b0bec79e50881557cc35e9cf
shubham_97 <shubham.agarwal@clix.capital>
2021-04-05 01:45:59 +0530

    dummy data json added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,42 @@
 export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
 export const DAEMON = '@@saga-injector/daemon';
 export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
+
+export const tableData = [
+  {
+    name: 'Test Whatsapp',
+    region: 'US',
+    createdOn: 1559807714999,
+    price: 'Price info of Test Whatsapp',
+    csv: 'Some CSV link for Whatsapp',
+    report: 'Some report link for Whatsapp',
+    image_url: '/static/images/dashboard/Bitmap.png',
+  },
+  {
+    name: 'Super Jewels Quest',
+    region: 'CA, FR',
+    createdOn: 1559806715124,
+    price: 'Price info of Super Jewels Quest',
+    csv: 'Some CSV link for Super Jewels Quest',
+    report: 'Some report link for Super Jewels Ques',
+    image_url: '/static/images/dashboard/BitmapPopup.png',
+  },
+  {
+    name: 'Mole Slayer',
+    region: 'FR',
+    createdOn: 1559806711124,
+    price: 'Price info of Mole Slayer',
+    csv: 'Some CSV link for Mole Slayer',
+    report: 'Some report link for Mole Slayer',
+    image_url: '/static/images/dashboard/Bitmap3.png',
+  },
+  {
+    name: 'Mancala Mix',
+    region: 'JP',
+    createdOn: 1559806680124,
+    price: 'Price info of Mancala Mix',
+    csv: 'Some CSV link for Mancala Mix',
+    report: 'Some report link for Mancala Mix',
+    image_url: '/static/images/dashboard/Bitmap4.png',
+  },
+];
