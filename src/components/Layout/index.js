a45946fc34172659422201716b564f603cb7c8df
shubham_97 <shubham.agarwal@clix.capital>
2021-04-05 01:49:53 +0530

    Layout updated

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,19 +4,24 @@ import PropTypes from 'prop-types';
 import Header from 'components/Header';
 import Banner from 'components/Banner';
 import Footer from 'components/Footer';
+import styled from '@emotion/styled';
+
+const MainRoot = styled('main')`
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 16px;
+  max-width: 1024px;
+`;
 
 function Layout({ children }) {
   return (
     <>
       <Header />
-
-      <main>
+      <MainRoot>
         <Banner />
-
         {children}
-      </main>
-
-      <Footer />
+      </MainRoot>
+      {/* <Footer /> */}
     </>
   );
 }
